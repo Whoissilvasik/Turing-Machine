782f9568b39f62b94a005dba5b193d866469353b
Silvestr Vanagas <silvestr.vanagas@gmail.com>
2024-06-20 11:29:07 +0300

    Update Touring machine C++.cpp
    I've added seccond tape

--- Touring machine C++/Touring machine C++/Touring machine C++.cpp
+++ Touring machine C++/Touring machine C++/Touring machine C++.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <set>
@@ -7,36 +7,38 @@
 
 using namespace std;
 
-struct juosta
+struct juostos
 {
     int poz;
     vector <char> simbolis;
 };
 
-struct komanda
+struct komandos
 {
     string Pr_bus;
-    char symbol;
-    char new_symbol;
-    char direction;
+    char* symbol;
+    char* new_symbol;
+    char* direction;
     string Pb_bus;
 };
 
 bool keep_going(true);
 void Order_stop(int signum);
-void Spausdinimas(juosta T);
+void Spausdinimas(int ind, juostos T[]);
 string Bus_gav(int& i, string& rules);
-void Komandos_gav(int& i, string& rules, komanda& cmd);
+void Komandos_gav(int n, int& i, string& rules, char X[]);
 
 int main()
 {
     signal(SIGINT, Order_stop); // Ctrl + C
+    int Gal_kiek;
     int komandu_kiek = 0;
+    int found = 0;
     string Dab_bus;
     set <string> State;
     char bad_simbol;
-    juosta t;
-    komanda* cmd;
+    juostos* juosta;
+    komandos* cmd;
     bool Ar_pabaiga = true;
 
     string file_name;
@@ -49,14 +51,24 @@ int main()
         return 1;
     }
 
+    fd >> Gal_kiek;
+    juosta = new juostos[Gal_kiek];
+
     string line;
-    fd >> line;
-    for (char c : line)
+
+    for (int i = 0; i < Gal_kiek; i++)
     {
-        t.simbolis.push_back(c);
+        fd >> line;
+        for (int j = 0; j < line.size(); j++)
+        {
+            juosta[i].simbolis.push_back(line[j]);
+        }
     }
-    fd >> t.poz;
-    t.poz--;
+    for (int i = 0; i < Gal_kiek; i++)
+    {
+        fd >> juosta[i].poz;
+        juosta[i].poz--;
+    }
 
     string temp;
     string rules;
@@ -65,7 +77,7 @@ int main()
     {
         rules += temp + " ";
         komandu_kiek++;
-        for (int i = 1; i < 4 + 2; i++)
+        for (int i = 1; i < 3 * Gal_kiek + 2; i++)
         {
             fd >> temp;
             rules += temp + " ";
@@ -73,86 +85,117 @@ int main()
     }
     fd.close();
 
-    komandu_kiek /= 6;
+    komandu_kiek /= (3 * Gal_kiek + 2);
 
-    cmd = new komanda[komandu_kiek];
+    cmd = new komandos[komandu_kiek];
 
-    int t_index = 0;
-    for (int i = 0; i < rules.size() && t_index < komandu_kiek;)
+    for (int i = 0; i < komandu_kiek; i++)
     {
-        cmd[t_index].Pr_bus = Bus_gav(i, rules);
-        State.insert(cmd[t_index].Pr_bus);
-        Komandos_gav(i, rules, cmd[t_index]);
-        t_index++;
+        cmd[i].symbol = new char[Gal_kiek];
+        cmd[i].new_symbol = new char[Gal_kiek];
+        cmd[i].direction = new char[Gal_kiek];
+    }
+
+    int t = 0;
+    for (int i = 0; i < rules.size() && t < komandu_kiek;)
+    {
+        cmd[t].Pr_bus = Bus_gav(i, rules);
+        State.insert(cmd[t].Pr_bus);
+        Komandos_gav(Gal_kiek, i, rules, cmd[t].symbol);
+        Komandos_gav(Gal_kiek, i, rules, cmd[t].new_symbol);
+        Komandos_gav(Gal_kiek, i, rules, cmd[t].direction);
+        cmd[t].Pb_bus = Bus_gav(i, rules);
+        t++;
     }
 
     Dab_bus = cmd[0].Pr_bus;
 
     do {
-        bool found = false;
+        found = 0;
         for (int i = 0; i < komandu_kiek; i++)
         {
-            if (Dab_bus == cmd[i].Pr_bus && cmd[i].symbol == t.simbolis[t.poz])
+            if (Dab_bus == cmd[i].Pr_bus)
             {
-                Dab_bus = cmd[i].Pb_bus;
-                t.simbolis[t.poz] = cmd[i].new_symbol;
-
-                if (cmd[i].direction == 'R')
+                bool match = true;
+                for (int j = 0; j < Gal_kiek; j++)
                 {
-                    t.poz++;
-                    if (t.poz == t.simbolis.size())
+                    if (cmd[i].symbol[j] != juosta[j].simbolis[juosta[j].poz])
                     {
-                        t.simbolis.push_back(' ');
-                    }
-                }
-                else if (cmd[i].direction == 'L')
-                {
-                    if (t.poz == 0)
-                    {
-                        t.simbolis.insert(t.simbolis.begin(), ' ');
-                    }
-                    else
-                    {
-                        t.poz--;
+                        match = false;
+                        bad_simbol = juosta[j].simbolis[juosta[j].poz];
+                        break;
                     }
                 }
 
-                cout << "J: ";
-                Spausdinimas(t);
-                cout << endl;
+                if (match)
+                {
+                    Dab_bus = cmd[i].Pb_bus;
+                    for (int j = 0; j < Gal_kiek; j++)
+                    {
+                        juosta[j].simbolis[juosta[j].poz] = cmd[i].new_symbol[j];
 
-                found = true;
-                break;
+                        if (cmd[i].direction[j] == 'R')
+                        {
+                            juosta[j].poz++;
+                            if (juosta[j].poz == juosta[j].simbolis.size())
+                            {
+                                juosta[j].simbolis.push_back(' ');
+                            }
+                        }
+                        else if (cmd[i].direction[j] == 'L')
+                        {
+                            if (juosta[j].poz == 0)
+                            {
+                                juosta[j].simbolis.insert(juosta[j].simbolis.begin(), ' ');
+                            }
+                            else
+                            {
+                                juosta[j].poz--;
+                            }
+                        }
+
+                        cout << "J" << j + 1 << ":";
+                        Spausdinimas(j, juosta);
+                    }
+                    cout << endl;
+                    found = 1;
+                    break;
+                }
             }
         }
-        if (!found)
-        {
-            bad_simbol = t.simbolis[t.poz];
-            break;
-        }
-    } while (keep_going && State.find(Dab_bus) != State.end());
+    } while (found);
 
-    if (State.find(Dab_bus) == State.end())
+    if (State.find(Dab_bus) != State.end())
     {
         Ar_pabaiga = false;
     }
 
     if (!Ar_pabaiga)
-        cout << "No state [" << Dab_bus << "] for this simbol [" << bad_simbol << "]" << endl;
+        cout << "Nera busenos [" << Dab_bus << "] siam simboliui [" << bad_simbol << "]" << endl;
     else
-        cout << "Final state [" << Dab_bus << "] done!" << endl << endl;
+        cout << "Pasekta paskutine busena [" << Dab_bus << "] done!" << endl << endl;
 
-    cout << "Tape: ";
-    Spausdinimas(t);
+    for (int i = 0; i < Gal_kiek; i++)
+    {
+        cout << "Juosta " << i + 1 << ": ";
+        Spausdinimas(i, juosta);
+    }
 
+    for (int i = 0; i < komandu_kiek; i++)
+    {
+        delete[] cmd[i].symbol;
+        delete[] cmd[i].new_symbol;
+        delete[] cmd[i].direction;
+    }
     delete[] cmd;
+    delete[] juosta;
 
     return 0;
 }
 
-void Spausdinimas(juosta T)
+void Spausdinimas(int i, juostos juost[])
 {
-    for (const auto& symbol : T.simbolis)
+    for (const auto& symbol : juost[i].simbolis)
     {
         cout << symbol;
     }
@@ -171,15 +214,13 @@ string Bus_gav(int& i, string& rules)
     return BUSENA;
 }
 
-void Komandos_gav(int& i, string& rules, komanda& cmd)
+void Komandos_gav(int Gal_kiek, int& i, string& rules, char cmd[])
 {
-    cmd.symbol = rules[i];
-    i += 2;
-    cmd.new_symbol = rules[i];
-    i += 2;
-    cmd.direction = rules[i];
-    i += 2;
-    cmd.Pb_bus = Bus_gav(i, rules);
+    for (int l = 0; l < Gal_kiek; l++)
+    {
+        cmd[l] = rules[i];
+        i += 2;
+    }
 }
 
 void Order_stop(int signum)
